6524da80cc236f088a32b721596b737bed28db1b
AndresCaro <andresf.caro@estudiante.uam.es>
2023-03-24 12:00:05 +0100

    Add OpenMP Time

--- theory/par_b.c
+++ theory/par_b.c
@@ -9,10 +9,17 @@ void main(){
   float a[N];
 
   double time=0.;
+  double time2=0.;
+
   clock_t begin, end;
   begin = clock();
   
+
   a[0] = STARTVALUE;
+
+  double start;
+  double send;
+  start = omp_get_wtime();
 
 #pragma omp parallel 
   {
@@ -27,14 +34,20 @@ void main(){
       printf("Thread %d: a[%d]=%.1f \n",ID,i,a[i]);
     }
   } // end of parallel section
+  
+
+  printf("Work took %f seconds\n", end - start);
 
   printf("\n Results:\n");
   for(int i=0; i<N; i++){
     printf("a[%d]=%.1f \n",i,a[i]);
   }
 
+  send = omp_get_wtime();
   end = clock();
-  time = 1000.*(end - begin)/CLOCKS_PER_SEC; //ms
-  printf("\n Time=%E ms \n",time);    
+  time = 1000.*(end - begin)/CLOCKS_PER_SEC; //s
+  printf("\n Time with Clock() = %E ms \n",time);    
+  time2 = 1000.*(send - start); //s
+  printf("Time with omp_get_wtime() = %E ms\n", time2);
 }
   
